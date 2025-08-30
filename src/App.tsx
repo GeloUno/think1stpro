62e9e2b28ec2c064211f24b73cec7dd79d569e92
Grzegorz Kędziora <geloke59@gmail.com>
2025-08-30 15:33:59 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <p className="text-3xl font-bold text-emerald-600">think1stpro</p>
+      </div>
+    </>
+  );
+}
+
+export default App;

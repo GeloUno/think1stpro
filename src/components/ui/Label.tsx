57ef47e64b218750c289f4d315516b83f4d3533e
Grzegorz Kędziora <geloke59@gmail.com>
2025-08-30 21:52:03 +0200

    add label

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,17 @@
+interface ILabelProps {
+  label: string;
+  htmlForm?: string;
+}
+
+function Label({ label, htmlForm }: ILabelProps) {
+  return (
+    <label
+      htmlFor={htmlForm}
+      className="block text-xs font-medium text-primary-text"
+    >
+      {label}
+    </label>
+  );
+}
+
+export default Label;

e0c5810f2d392cd65001770acbc7032b37ae7271
Grzegorz Kędziora <geloke59@gmail.com>
2025-09-01 00:23:04 +0200

    add hook context form

---
+++ src/hooks/useFormContext.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { FormContext } from '../context/FormContext';
+
+export default function useFormContext() {
+  const ctx = useContext(FormContext);
+  if (!ctx) {
+    throw new Error('useFormContext must be used inside <FormProvider>');
+  }
+  return ctx;
+}
